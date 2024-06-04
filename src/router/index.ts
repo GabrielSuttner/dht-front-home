209e022ace8ff5d5304b2259762984021dba1ce5
gabriel suttner <gabrielsuttner@gmail.com>
2024-06-04 17:00:41 -0600

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,4 +22,10 @@ const router = createRouter({
   ]
 });
 
+router.beforeEach((to, from, next) => {
+  console.log('Before each route');
+  console.log(to, from);
+  next();
+});
+
 export default router;
